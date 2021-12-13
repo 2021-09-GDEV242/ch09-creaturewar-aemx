757f9e5c7ea2ab0153501e11c40be02dfed6cb51
Spencer Gunning <swcgunning@gmail.com>
2021-12-12 20:51:12 -0500

    Implement core functionality for Creature class

--- Creature.java
+++ Creature.java
@@ -1,33 +1,32 @@
-
 /**
  * Abstract class Creature - 
  * The creature is the main class from which all other battling creatures derive.
  * It is the creature's job to maintain current health values in response to 
  * requests to takeDamage. It also must report if
  * the creature is alive or knocked out. The creature is also responsible for calculating
- * damage delivered based on the creature's strength (1 to str) 
+ * damage delivered based on the creature's strength (1 to strength) 
  * 
- * @author Crosbie
- * @version 2020-10 v1.0
+ * @author Spencer Gunning
+ * @version 2021.12.12
  */
-// we will learn what the abstract keyword does in a later chapter
+
 public abstract class Creature
 {
-    private int str;        // The strength of this creature
-    private int max_hp;     // The maximum hit points the creature can have (used if healing enabled)
-    private int hp;         // the current numberof hit points the creature has
-    
+    private int strength;        // The strength of this creature
+    private int maxHealth;     // The maximum hit points the creature can have (used if healing enabled)
+    private int health;         // the current numberof hit points the creature has
+
     /**
      * default constructor - this should never actually run.
      * It is the job of dervived classes to set up the proper number of hit points and 
      * strength for the subclass
      */
     public Creature (){
-        str=10;
-        hp=10;
-        max_hp = hp;
+        strength = 10;
+        health = 10;
+        maxHealth = health;
     }
-    
+
     /**
      * Create a creature with a given strength and hit point level. 
      * Store max hitpoints to allow for healing to be implemented later
@@ -37,46 +36,49 @@ public abstract class Creature
      * @param hp the health of the creature at the start of the simulation, and the current health levels during battle
      */
     public Creature (int str, int hp) {
-       //implement this
+       strength = str;
+       health = hp;
+       maxHealth = health;
     }
-    
-    
+
     /**
      * Allows a creature to determine how much damage it is causing in this round of battle
-     * @return a value between 1 and str to be used to cause damage to another creature
+     * @return a value between 1 and strength to be used to cause damage to another creature
      */
     public int attack(){
-        // TODO: implement a damage method
-        return 0;
+        return Randomizer.nextInt(strength);
     }
-    
-    
-    /**
-     * Is this creature still capable of fighting?
-     * @return true when current hit point level is greater than zero
-     */
-    public boolean isAlive() {
-        // TODO: implement a method to report if the creature yet lives
-        return false; //change this
-    }
-    
-    /**
-     * Is this creature knockedOut?
-     * @return true when current hit point level is less than or equal to zero
-     */
-    public boolean isKnockedOut() {
-        //TODO: implement a method to report if the creature has been killed
-        return false; //change this
-    }
-    
-    
+
     /**
      * takeDamage receives a value for the amount of damage to subtract from 
      * the current total of hit points
      * @param damage value to remove from hit point count
      */
     public void takeDamage(int damage) {
-        // TODO: implement this
+        health = health- damage;
     }
-    
+
+    /**
+     * Is this creature still capable of fighting?
+     * @return true when current hit point level is greater than zero
+     */
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    /**
+     * Is this creature knockedOut?
+     * @return true when current hit point level is less than or equal to zero
+     */
+    public boolean isKnockedOut() {
+        return health <= 0;
+    }
+
+    /**
+     * How many hit points does this creature have?
+     * @return the current health value of the creature 
+     */
+    public int getHealth() {
+        return health;
+    }
 }
